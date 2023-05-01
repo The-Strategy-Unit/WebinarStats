734c48be23edf4b6cf4da161d7328c88093a9e5e
Craig Parylo <craigparylo@Craigs-MacBook-Pro.local>
2023-05-01 12:45:48 +0100

    add an alternate Shiny app based on the ShinyDashboard framework

---
+++ app_v2/app.R
@@ -0,0 +1,361 @@
+#' -----------------------------------------------------------------------------
+#' WEBINAR STATS (V2)
+#' 
+#' A Shiny app to explore statistics following an online webinar
+#' -----------------------------------------------------------------------------
+
+# libraries --------------------------------------------------------------------
+library(shiny)                    # shiny core
+library(shinydashboard)           # layout and display functions
+library(shinyTime)                # allows for a time input widget
+library(here)                     # localised file references
+library(plotly)                   # charts
+
+## user-defined functions ----
+source(here('functions_script.R'))
+
+# ui ---------------------------------------------------------------------------
+ui <- dashboardPage(
+  skin = 'yellow',
+  
+  ## header --------------------------------------------------------------------
+  dashboardHeader(
+    title = 'Webinar Stats'
+    # title = tags$a(
+    #   href='http://mycompanyishere.com',
+    #   tags$img(src='tsu_logo_yellow_screen_transparent.png'),
+    #   width = 10
+    # )
+  ),
+  
+  
+  # dashboardPage(
+  #   dashboardHeader(title = tags$a(href='http://mycompanyishere.com',
+  #                                  tags$img(src='logo.png')))
+    
+  
+  
+  ## sidebar -------------------------------------------------------------------
+  dashboardSidebar(
+    sidebarMenu(
+      
+      ### import ----
+      menuItem(
+        text = 'Import',
+        tabName = 'file_import',
+        icon = icon('file-import')
+      ),
+      
+      ### stats ----
+      menuItem(
+        text = 'Statistics',
+        tabName = 'statistics',
+        icon = icon('table')
+      ),
+      
+      ### chart ----
+      menuItem(
+        text = 'Chart',
+        tabName = 'other',
+        icon = icon('ellipsis')
+      )
+    )
+  ),
+  
+  # body -----------------------------------------------------------------------
+  dashboardBody(
+    
+    tags$img(
+      src = 'tsu_logo_yellow_screen_transparent.png',
+      align = 'right',
+      height = 70
+    ),
+
+    tabItems(
+      ## import ----------------------------------------------------------------
+      tabItem(
+        tabName = 'file_import',
+        
+        ### title and instructions
+        fluidRow(
+          column(
+            width = 12,
+            h1('File import'),
+            'Import your attendance file and configure start and end times',
+          )
+        ),
+        
+        ### import configuration box ----
+        fluidRow(
+          
+          #### file --
+          box(
+            title = 'File import',
+            width = 6,
+            fileInput(
+              inputId = 'upload',
+              label = 'Select the csv file containing the attendance data for your event:',
+              accept = '.csv'
+            )
+          ),
+          
+          #### date --
+          box(
+            title = 'Date',
+            width = 2,
+            
+            dateInput(
+              inputId = 'live_event_date',
+              label = 'Event date'
+            )
+          ),
+          
+          #### time --
+          box(
+            title = 'Time',
+            width = 4,
+            
+            column(
+              width = 6,
+              timeInput(
+                inputId = 'start',
+                label = 'Start time',
+                value = strptime('09:00:00', '%T'),
+                minute.steps = 15
+              )
+            ),
+            
+            column(
+              width = 6,
+              timeInput(
+                inputId = 'end',
+                label = 'End time',
+                value = strptime('17:00:00', '%T'),
+                minute.steps = 15
+              )
+            )
+          )
+          
+        ),
+        
+        ### import feedback chart ----
+        fluidRow(
+          plotlyOutput('file_import_calibration_chart')
+        )
+      ),
+      
+      ## statistics ------------------------------------------------------------
+      tabItem(
+        tabName = 'statistics',
+        
+        ### title and instructions
+        fluidRow(
+          column(
+            width = 12,
+            h1('Statistics'),
+          )
+        ),
+        
+        fluidRow(
+          column(width = 12, 'Here are some interesting facts about your event:'),
+          valueBoxOutput(outputId = 'number_of_attendees', width = 6),
+          valueBoxOutput(outputId = 'averagetime', width = 6),
+        ),
+        
+        fluidRow(
+          column(width = 12, 'Timeliness of attendances'),
+          valueBoxOutput(outputId = 'attend_less_than_15', width = 4),
+          valueBoxOutput(outputId = 'attend_more_than_45', width = 4),
+          valueBoxOutput(outputId = 'joined_after_15mins', width = 4),
+        ),
+        
+        fluidRow(
+          column(width = 12, 'How people attended'),
+          valueBoxOutput(outputId = 'attended_on_computer', width = 6),
+          valueBoxOutput(outputId = 'attended_on_mobile', width = 6),
+        )
+
+      ),
+      
+      ## other -----------------------------------------------------------------
+      tabItem(
+        tabName = 'other'
+      )
+      
+      
+      
+    )
+  )
+)
+
+
+# server -----------------------------------------------------------------------
+server <- function(input, output) {
+  
+
+  
+  
+  # import ---------------------------------------------------------------------
+  # read the imported file
+  df <- reactive({
+    
+    # require a file to be uploaded before doing this
+    req(input$upload)
+    
+    # work out how to read the file
+    ext <- tools::file_ext(input$upload$name)
+    switch(
+      ext,
+      csv = vroom(input$upload$datapath, delim = ',', col_types = cols(col_character())),
+      tsv = vroom(input$upload$datapath, delim = '\t', col_types = cols(col_character())),
+      validate('Invalid file; please upload a .csv or .tsv file')
+    ) |> 
+      # process the file
+      process_imported_file()
+  })
+  
+  ## estimate start / end dates -------------------------------------------------
+  observeEvent(input$upload, {
+    
+    # require a file to be uploaded and processed
+    req(df)
+    
+    # update the ui with estimated start and end datetimes
+    updateDateInput(
+      inputId = 'live_event_date',
+      value = median(df()$joined_datetime_rounded, na.rm = T)
+    )
+    updateTimeInput(
+      session = getDefaultReactiveDomain(),
+      inputId = 'start',
+      value = median(df()$joined_datetime_rounded, na.rm = T)
+    )
+    updateTimeInput(
+      session = getDefaultReactiveDomain(),
+      inputId = 'end',
+      value = quantile(df()$left_datetime_rounded, probs = 0.75, na.rm = T)
+    )
+  })
+  
+  ## file import calibration chart ---------------------------------------------
+  # get attendee data
+  df_attendees <- reactive({
+    req(df(), input$live_event_date, input$start, input$end)
+    
+    df() |> 
+      get_event_attendees(
+        event_start_datetime = get_event_datetime(input$live_event_date, input$start),
+        event_end_datetime = get_event_datetime(input$live_event_date, input$end)
+      )
+  })
+  
+  # get count of attendees per minute
+  df_attendees_per_minute <- reactive({
+    req(df_attendees(), input$live_event_date, input$start, input$end)
+    
+    df_attendees() |> 
+      get_attandance_count_per_minute(
+        start = get_event_datetime(input$live_event_date, input$start),
+        end = get_event_datetime(input$live_event_date, input$end)
+      )
+
+  })
+  
+  # render the chart of attendees per minute
+  output$file_import_calibration_chart <- renderPlotly({
+    req(df_attendees_per_minute())
+
+    plot_ly(
+      data = df_attendees_per_minute(),
+      x = ~minute,
+      y = ~attendees
+    ) |>
+      add_bars(marker = list(color = 'rgb(222,163,59)')) |> 
+      layout(
+        title = 'Number of attendees each minute',
+        bargap = 0.05,
+        xaxis = list(title = 'Time'),
+        yaxis = list(title = 'Number of attendees'),
+        font = list(family = 'Segoe UI, Helvetica, Arial, sans-serif')
+      )
+  })
+  
+  
+  # statistics -----------------------------------------------------------------
+  # get a tibble with calculated statistics
+  df_statistics <- reactive({
+    req(df_attendees())
+    calculate_attendance_statistic(df_attendees = df_attendees())
+  })
+  
+  # render stats boxes
+  fluidRow(
+    output$averagetime <- renderValueBox({
+      valueBox(
+        value = df_statistics()$mean_duration,
+        subtitle = 'Average time attending the event',
+        icon = icon('clock'),
+        color = 'yellow'
+      )
+    }),
+    
+    output$number_of_attendees <- renderValueBox({
+      valueBox(
+        value = df_statistics()$number_of_attendees,
+        subtitle = 'Attended the event',
+        icon = icon('people-group'),
+        color = 'yellow'
+      )
+    }),
+    
+    output$attend_less_than_15 <- renderValueBox({
+      valueBox(
+        value = df_statistics()$attend_less_than_15,
+        subtitle = 'Attended for less than 15 minutes',
+        icon = icon('stopwatch'),
+        color = 'green'
+      )
+    }),
+    
+    output$attend_more_than_45 <- renderValueBox({
+      valueBox(
+        value = df_statistics()$attend_more_than_45,
+        subtitle = 'Attended for more than 45 minutes',
+        icon = icon('people-line'),
+        color = 'green'
+      )
+    }),
+    
+    output$joined_after_15mins <- renderValueBox({
+      valueBox(
+        value = df_statistics()$joined_after_15mins,
+        subtitle = 'Joined after 15 minutes',
+        icon = icon('right-to-bracket'),
+        color = 'green'
+      )
+    }),
+    
+    output$attended_on_computer <- renderValueBox({
+      valueBox(
+        value = df_statistics()$attended_on_computer,
+        subtitle = 'Attended using a computer or laptop',
+        icon = icon('display'),
+        color = 'purple'
+      )
+    }),
+    
+    output$attended_on_mobile <- renderValueBox({
+      valueBox(
+        value = df_statistics()$attended_on_mobile,
+        subtitle = 'Attended on a mobile device',
+        icon = icon('mobile-screen-button'),
+        color = 'purple'
+      )
+    }),
+  )
+
+    
+}
+
+# run --------------------------------------------------------------------------
+shinyApp(ui = ui, server = server)
